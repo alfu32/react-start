322cd2dbcf1a54c5fa401b2ff59c2f830b6196a0
alfu32 <ioan.alferaru@gmail.com>
2019-05-28 06:16:46 +0200

    packaging

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {AppLayout,AppTitle,AppToolbar,AppSidebar,AppStatusbar,AppContent} from './components/app-layout.component';
+//import { AppLayout, AppTitle, AppToolbar, AppSidebar, AppStatusbar, AppContent } from '../node_modules/@sbefmp/core/src';
+
+import { AppLayout, AppTitle, AppToolbar, AppSidebar, AppStatusbar, AppContent } from '@sbefmp/core';
+//import { AppLayout, AppTitle, AppToolbar, AppSidebar, AppStatusbar, AppContent } from './components';
+//import '@sbefmp/core/dist/index.css';
 
 console.log('Hello from tsx!')
 
@@ -15,10 +19,12 @@ ReactDOM.render(
             <button>clickme</button>
         </AppToolbar>
         <AppSidebar>
-            {sidebarContent.map( s => <a href={'#'+s}>{s}</a> )}
+            {sidebarContent.map( (s,i) => <div key={'div.'+i}><a href={'#' + s}>{s}</a></div> )}
+            coucou
         </AppSidebar>
         <AppStatusbar>
             {status}
+            coucou
         </AppStatusbar>
         <AppContent>
             <h4>It Works</h4>
